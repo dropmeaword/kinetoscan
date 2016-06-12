f663e3eece3f201944a2613af8d3daf750b9dd3c
Luis Fernandez <dropmeaword@gmail.com>
2016-06-12 15:13:51 +0200

    removed references to pyboard libraries

--- python/imu/fusion.py
+++ python/imu/fusion.py
@@ -6,8 +6,10 @@
 # V0.5 angles method replaced by heading pitch and roll properties
 # V0.4 calibrate method added
 
-import pyb 
+import pyb
 from math import sqrt, atan2, asin, degrees, radians
+import datetime
+
 '''
 Supports 6 and 9 degrees of freedom sensors. Tested with InvenSense MPU-9150 9DOF sensor.
 Source https://github.com/xioTechnologies/Open-Source-AHRS-With-x-IMU.git
@@ -66,7 +68,7 @@ class Fusion(object):
         ax, ay, az = accel                  # Units G (but later normalised)
         gx, gy, gz = (radians(x) for x in gyro) # Units deg/s
         if self.start_time is None:
-            self.start_time = pyb.micros()  # First run
+            self.start_time = time.clock()  # First run
         q1, q2, q3, q4 = (self.q[x] for x in range(4))   # short name local variable for readability
         # Auxiliary variables to avoid repeated arithmetic
         _2q1 = 2 * q1
@@ -110,8 +112,8 @@ class Fusion(object):
         qDot4 = 0.5 * (q1 * gz + q2 * gy - q3 * gx) - self.beta * s4
 
         # Integrate to yield quaternion
-        deltat = pyb.elapsed_micros(self.start_time) / 1000000
-        self.start_time = pyb.micros()
+        deltat = time.clock() - self.start_time
+        self.start_time = time.clock()
         q1 += qDot1 * deltat
         q2 += qDot2 * deltat
         q3 += qDot3 * deltat
@@ -124,7 +126,7 @@ class Fusion(object):
         ax, ay, az = accel                  # Units irrelevant (normalised)
         gx, gy, gz = (radians(x) for x in gyro)  # Units deg/s
         if self.start_time is None:
-            self.start_time = pyb.micros()  # First run
+            self.start_time = time.clock()  # First run
         q1, q2, q3, q4 = (self.q[x] for x in range(4))   # short name local variable for readability
         # Auxiliary variables to avoid repeated arithmetic
         _2q1 = 2 * q1
@@ -205,8 +207,8 @@ class Fusion(object):
         qDot4 = 0.5 * (q1 * gz + q2 * gy - q3 * gx) - self.beta * s4
 
         # Integrate to yield quaternion
-        deltat = pyb.elapsed_micros(self.start_time) / 1000000
-        self.start_time = pyb.micros()
+        deltat = time.clock() - self.start_time
+        self.start_time = time.clock()
         q1 += qDot1 * deltat
         q2 += qDot2 * deltat
         q3 += qDot3 * deltat
